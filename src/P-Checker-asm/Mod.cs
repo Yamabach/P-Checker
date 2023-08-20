9ed945ef2a4ba57d82aafb897bc9d2c47189a804
Yamabach <yamabach.main@gmail.com>
2023-08-20 12:29:24 +0900

    added a function that change the regulation, fixed some bugs

--- src/P-Checker-asm/Mod.cs
+++ src/P-Checker-asm/Mod.cs
@@ -1,10 +1,12 @@
 using Modding;
+using Modding.Serialization;
 using System;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using UnityEngine;
 using System.Linq;
 using spaar.ModLoader.PCUI;
+using System.Xml.Serialization;
 
 namespace PCheckerSpace
 {
@@ -13,7 +15,7 @@ namespace PCheckerSpace
 		public static GameObject PCmod;
 		public override void OnLoad()
 		{
-			PCmod = new GameObject("4th P1GP regulation checker");
+			PCmod = new GameObject("P1GP Regulation Checker");
 			Log("Pチェッカーをロードしました");
 			SingleInstance<PCGUI>.Instance.transform.parent = PCmod.transform;
 			SingleInstance<BlockController.BlockSelector>.Instance.transform.parent = PCmod.transform;
@@ -41,15 +43,11 @@ namespace PCheckerSpace
 		public static BlockBehaviour picked;
 		private bool OpenURL = false;
 		private Machine machine = Machine.Active();
-		public override string Name
-		{
-			get
-			{
-				return "P Chacker GUI";
-			}
-		}
-		public const int MachineBlockMax = 200;
+		public override string Name => "P Chacker GUI";
 		public const int MachineSkinMax = 2;
+		/// <summary>
+		/// ブロックの総数を数える
+		/// </summary>
 		public int TotalBlock
 		{
 			get
@@ -67,23 +65,43 @@ namespace PCheckerSpace
 		}
 		private bool showDuringSimulation = true;
 		private bool minimizeUI = false;
-		private bool WasInGlobalPlayMode = false;
+		private bool wasInGlobalPlayMode = false;
 		private bool IsOK = false;
 		private AudioSource audioSource;
-		public static ModAudioClip SEPankoro, SENG;
+		public static ModAudioClip SEPankoro;
+		public static ModAudioClip SENG;
 		private bool PlaySound = true;
-		public GUIStyle StyleOk, StyleNg;
+		public GUIStyle StyleOk;
+		public GUIStyle	StyleNg;
+		/// <summary>
+		/// 回ごとのレギュ
+		/// </summary>
+		public XMLDeserializer.P1GPRegulations Regulations;
+		/// <summary>
+		/// 現在表示しているレギュの回
+		/// </summary>
+		private int currentRegulationCount = 6;
+		public bool AllowRocketExceed => Regulations.Find(currentRegulationCount).AllowRocketExceed;
 
 		public void Awake()
         {
+			// AudioSource取得
 			if (audioSource == null)
             {
 				audioSource = GetComponent<AudioSource>() ?? gameObject.AddComponent<AudioSource>();
 			}
+
+			// レギュ取得
+			Regulations = XMLDeserializer.Deserialize();
+			currentRegulationCount = Regulations.P1GPRegulation.Max(r => r.Count);
+
+			// GUIのテキストスタイルの初期化
 			StyleOk = new GUIStyle();
 			StyleOk.normal.textColor = Color.white;
 			StyleNg = new GUIStyle();
 			StyleNg.normal.textColor = Color.red;
+
+			// その他初期化
 			windowId = ModUtility.GetWindowId();
 			machine = Machine.Active();
         }
@@ -101,19 +119,16 @@ namespace PCheckerSpace
 			if (OpenURL)
 			{
 				OpenURL = false;
-				//Application.OpenURL("https://docs.google.com/spreadsheets/d/1oEi-RUSannCdbTHx1yuU1mhQcdUu-1u46QdpQjEo6Hg/edit?usp=sharing"); // 第4回
-				Application.OpenURL("https://docs.google.com/spreadsheets/d/1KBizrmb2volLAelfyXzKeEzKSntdt8GG4n-w8pF8J-E/edit?usp=sharing"); // 第5回
+
 				//公式レギュレーションのスプシを開く
+				Application.OpenURL(Regulations.Find(currentRegulationCount).WebLink);
 			}
-			if (minimizeUI)
-			{
-				windowRect.size = new Vector2(120f, 100f);
-			}
-			else 
-			{
-				windowRect.size = new Vector2(120f, 100f);
-			}
-			if (StatMaster.InGlobalPlayMode && !WasInGlobalPlayMode && PlaySound)
+
+			// サイズを常に小さくなるようにする
+			windowRect.size = new Vector2(120f, 100f);
+
+			// シミュ開始時にルールを満たしたかどうかでSEを鳴らす
+			if (StatMaster.InGlobalPlayMode && !wasInGlobalPlayMode && PlaySound)
             {
 				if (IsOK)
 				{
@@ -128,11 +143,11 @@ namespace PCheckerSpace
 					//Mod.Log("NG");
 				}
 			}
-			WasInGlobalPlayMode = StatMaster.InGlobalPlayMode;
+			wasInGlobalPlayMode = StatMaster.InGlobalPlayMode;
 		}
 		public void OnGUI()
 		{
-			if (machine == null)
+			if (machine is null)
             {
 				machine = Machine.Active();
             }
@@ -141,66 +156,79 @@ namespace PCheckerSpace
 			if (!StatMaster.isMainMenu && !hide)
 			{
 				if (machine.isSimulating && !showDuringSimulation) { return; }
-				windowRect = GUILayout.Window(windowId, windowRect, Mapper, "Pチェッカー");
+				windowRect = GUILayout.Window(windowId, windowRect, Mapper, $"Pチェッカー（第{currentRegulationCount}回用）");
 			}
 		}
 		public void Mapper(int windowId)
 		{
-			bool flagWhole, flagBomb, flagFlyingBlock, flagWaterCannon, flagShrapnelCannon, flagRocket, flagForbidden; //禁止・制限ブロック
-			bool flagScale, flagPower, flagSkin; //スケールされたブロック、違法にコピペされたブロック
+			// 禁止・制限ブロック
+			List<XMLDeserializer.Block> MandatoryBlocks = Regulations.MandatoryBlocks(currentRegulationCount);
+			List<XMLDeserializer.Block> LimitedBlocks = Regulations.LimitedBlocks(currentRegulationCount);
+			List<XMLDeserializer.Block> ForbiddenBlocks = Regulations.ForbiddenBlocks(currentRegulationCount);
+			List<bool> FlagsMandatory = new bool[MandatoryBlocks.Count].ToList();
+			List<bool> FlagsLimited = new bool[LimitedBlocks.Count].ToList();
+			bool flagForbidden = false;
+
+			// スケールされたブロック、違法にコピペされたブロック
+			bool flagScale, flagPower, flagSkin;
+			int total = TotalBlock;
+			int max = Regulations.Find(currentRegulationCount).MachineBlockMax;
+			bool flagWhole = total <= max;
 			if (!minimizeUI)
 			{
-				GUILayout.BeginHorizontal(); GUILayout.Label("総ブロック数", (TotalBlock <= MachineBlockMax ? StyleOk : StyleNg)); GUILayout.FlexibleSpace(); GUILayout.Label(TotalBlock.ToString() + "/" + MachineBlockMax, (TotalBlock <= MachineBlockMax ? StyleOk : StyleNg)); GUILayout.EndHorizontal();
+				GUILayout.BeginHorizontal();
+				GUILayout.Label($"総ブロック数", flagWhole ? StyleOk : StyleNg);
+				GUILayout.FlexibleSpace();
+				GUILayout.Label($"{total}/{max}", flagWhole ? StyleOk : StyleNg);
+				GUILayout.EndHorizontal();
 			}
-			flagWhole = TotalBlock <= MachineBlockMax;
-			flagBomb = ShowBlockNumber("ボム", (int)BlockType.Bomb, MachineBlockMax, 4);
-			flagFlyingBlock = ShowBlockNumber("フライングブロック", (int)BlockType.FlyingBlock, 20);
-			flagWaterCannon = ShowBlockNumber("ウォーターキャノン", (int)BlockType.WaterCannon, 20);
-			flagShrapnelCannon = ShowBlockNumber("榴散弾キャノン", (int)BlockType.ShrapnelCannon, 20);
-			flagRocket = ShowBlockNumber("ロケット", (int)BlockType.Rocket, 10);
-			flagForbidden = ShowBlockNumber("禁止ブロック", new int[]
-			{
-				(int)BlockType.Flamethrower,
-				(int)BlockType.Cannon, 
-				(int)BlockType.Vacuum, 
-				(int)BlockType.Crossbow, 
-				(int)BlockType.BuildSurface, 
-				52, 
-				(int)BlockType.ScalingBlock,
-				(int)BlockType.Propeller,
-				(int)BlockType.SmallPropeller,
-			});
 
-			flagScale = ShowBlockNumber("スケール変更", CheckType.Scale);
-			flagPower = ShowBlockNumber("コピペ使用", CheckType.Power);
+			// 必須ブロック
+			for (int i=0; i<MandatoryBlocks.Count; i++)
+            {
+				var block = MandatoryBlocks[i];
+				FlagsMandatory[i] = ShowBlockNumber(block.Name, block.Id, block.Max, block.Min);
+            }
+			// 制限ブロック
+			for (int i=0; i<LimitedBlocks.Count; i++)
+            {
+				var block = LimitedBlocks[i];
+				FlagsLimited[i] = ShowBlockNumber(block.Name, block.Id, block.Max, block.Min);
+            }
+			// 禁止ブロック
+			flagForbidden = ShowBlockNumber($"禁止ブロック", ForbiddenBlocks.Select(a => a.Id).ToArray());
+
+
+			flagScale = ShowBlockNumber($"スケール変更", CheckType.Scale);
+			flagPower = ShowBlockNumber($"コピペ使用", CheckType.Power);
 			flagSkin = ShowSkinNumber();
 
-			GUILayout.BeginHorizontal();
 			IsOK = flagWhole &&
-				flagBomb &&
-				flagFlyingBlock &&
-				flagWaterCannon &&
-				flagShrapnelCannon &&
-				flagRocket &&
+			    FlagsMandatory.All(f => f) &&
+				FlagsLimited.All(f => f) &&
 				flagForbidden &&
 				flagScale &&
 				flagPower &&
 				flagSkin;
-			GUILayout.Label("ブロック数総評", IsOK ? StyleOk : StyleNg); GUILayout.FlexibleSpace(); GUILayout.Label(IsOK ? "OK" : "NG", IsOK ? StyleOk : StyleNg);
+			GUILayout.BeginHorizontal();
+			GUILayout.Label($"ブロック数総評", IsOK ? StyleOk : StyleNg);
+			GUILayout.FlexibleSpace();
+			GUILayout.Label(IsOK ? $"OK" : $"NG", IsOK ? StyleOk : StyleNg);
 			GUILayout.EndHorizontal();
 
+			// ブロックのTransform表示
 			if (!minimizeUI)
 			{
 				GUILayout.Label("");
 				GUILayout.BeginHorizontal();
-				GUILayout.Label("選択中のブロック"); GUILayout.FlexibleSpace(); GUILayout.Label(picked != null ? picked.name : "-");
+				GUILayout.Label($"選択中のブロック"); GUILayout.FlexibleSpace(); GUILayout.Label(picked != null ? picked.name : "-");
 				GUILayout.EndHorizontal();
 				GUILayout.BeginHorizontal();
-				GUILayout.Label("ブロックの角度"); GUILayout.FlexibleSpace(); GUILayout.Label(picked != null ? picked.transform.rotation.eulerAngles.ToString() : "(-, -, -)");
+				GUILayout.Label($"ブロックの角度"); GUILayout.FlexibleSpace(); GUILayout.Label(picked != null ? picked.transform.rotation.eulerAngles.ToString() : "(-, -, -)");
 				GUILayout.EndHorizontal();
 
 				GUILayout.Label("");
-				GUILayout.Label("詳しいレギュレーションは、\n大会運営スプレッドシートをご覧ください");
+				GUILayout.Label($"詳しいレギュレーションは、\n大会運営スプレッドシートをご覧ください");
 			}
 			else
 			{
@@ -208,8 +236,10 @@ namespace PCheckerSpace
 				GUILayout.Label(picked != null ? picked.name : "-"); GUILayout.FlexibleSpace(); GUILayout.Label(picked != null ? picked.transform.rotation.eulerAngles.ToString() : "(-,-,-)");
 				GUILayout.EndHorizontal();
 			}
+
 			GUILayout.BeginHorizontal();
-			GUILayout.FlexibleSpace(); OpenURL = GUILayout.Button("ブラウザでルールを読む");
+			GUILayout.FlexibleSpace();
+			OpenURL = GUILayout.Button("ブラウザでルールを読む");
 			GUILayout.EndHorizontal();
 
 			GUILayout.BeginHorizontal();
@@ -230,6 +260,27 @@ namespace PCheckerSpace
 				GUILayout.Label("サウンド");
 				GUILayout.FlexibleSpace();
 				PlaySound = GUILayout.Toggle(PlaySound, "    ");
+				GUILayout.EndHorizontal();
+			}
+
+			if (!minimizeUI) {
+				// 第n回のルールに切り替える
+				GUILayout.BeginHorizontal();
+				for (int i = 0; i < Regulations.P1GPRegulation.Count; i++)
+				{
+					if (GUILayout.Button($"第{Regulations.P1GPRegulation[i].Count}回"))
+					{
+						currentRegulationCount = Regulations.P1GPRegulation[i].Count;
+						//Mod.Log($"第{currentRegulationCount}回");
+					}
+
+					// 3個ごとに改行
+					if (i % 3 == 2)
+                    {
+						GUILayout.EndHorizontal();
+						GUILayout.BeginHorizontal();
+                    }
+				}
 				GUILayout.EndHorizontal();
 			}
 
@@ -301,7 +352,7 @@ namespace PCheckerSpace
 					int num_scale = 0;
 					foreach (BlockBehaviour block in machine.BuildingBlocks)
 					{
-						if (block.transform.localScale != Vector3.one)
+						if (block.transform.localScale != UnityEngine.Vector3.one)
 						{
 							num_scale++;
 						}
@@ -310,7 +361,9 @@ namespace PCheckerSpace
 					if (!minimizeUI)
 					{
 						GUILayout.BeginHorizontal();
-						GUILayout.Label(Label, ret ? StyleOk : StyleNg); GUILayout.FlexibleSpace(); GUILayout.Label(num_scale.ToString(), ret ? StyleOk : StyleNg);
+						GUILayout.Label(Label, ret ? StyleOk : StyleNg);
+						GUILayout.FlexibleSpace();
+						GUILayout.Label(num_scale.ToString(), ret ? StyleOk : StyleNg);
 						GUILayout.EndHorizontal();
 					}
 					break;
@@ -324,7 +377,7 @@ namespace PCheckerSpace
 							continue;
                         }
 						CBB = block.GetComponent<BlockController.CustomBlockBehaviour>();
-						if (CBB == null)
+						if (CBB is null)
 						{
 							Mod.Error(block.name + "でCustomBlockBehaviourがnullです");
 							continue;
@@ -338,16 +391,20 @@ namespace PCheckerSpace
 					if (!minimizeUI)
 					{
 						GUILayout.BeginHorizontal();
-						GUILayout.Label(Label, ret ? StyleOk : StyleNg); GUILayout.FlexibleSpace(); GUILayout.Label(num_power.ToString(), ret ? StyleOk : StyleNg);
+						GUILayout.Label(Label, ret ? StyleOk : StyleNg);
+						GUILayout.FlexibleSpace();
+						GUILayout.Label($"{num_power}", ret ? StyleOk : StyleNg);
 						GUILayout.EndHorizontal();
 					}
 					break;
 				default:
-					ret = TotalBlock <= MachineBlockMax;
+					ret = TotalBlock <= Regulations.Find(currentRegulationCount).MachineBlockMax;
 					if (!minimizeUI)
 					{
 						GUILayout.BeginHorizontal();
-						GUILayout.Label(Label, ret ? StyleOk : StyleNg); GUILayout.FlexibleSpace(); GUILayout.Label(TotalBlock.ToString(), ret ? StyleOk : StyleNg);
+						GUILayout.Label(Label, ret ? StyleOk : StyleNg);
+						GUILayout.FlexibleSpace();
+						GUILayout.Label(TotalBlock.ToString(), ret ? StyleOk : StyleNg);
 						GUILayout.EndHorizontal();
 					}
 					break;
@@ -404,4 +461,100 @@ namespace PCheckerSpace
 			SENG = ModAudioClip.GetAudioClip("NG");
 		}
 	}
+	/// <summary>
+	/// Regulation.xmlを読み込むクラス
+	/// https://json2csharp.com/code-converters/xml-to-csharp により内部クラスを作成
+	/// </summary>
+	public class XMLDeserializer
+    {
+		[XmlRoot("Block")]
+		public class Block
+		{
+
+			[XmlAttribute(AttributeName = "name")]
+			public string Name { get; set; }
+
+			[XmlAttribute(AttributeName = "id")]
+			public int Id { get; set; }
+
+			[XmlAttribute(AttributeName = "min")]
+			public int Min { get; set; }
+
+			[XmlAttribute(AttributeName = "max")]
+			public int Max { get; set; }
+		}
+
+		[XmlRoot("P1GPRegulation")]
+		public class P1GPRegulation
+		{
+			/// <summary>
+			/// 個数が制限されるブロック
+			/// </summary>
+			[XmlElement("Block")]
+			public List<Block> Block { get; set; }
+			/// <summary>
+			/// ルール表へのリンク
+			/// </summary>
+			[XmlElement("link")]
+			public string WebLink { get; set; }
+			/// <summary>
+			/// 第n回のn
+			/// </summary>
+			[XmlAttribute("count")]
+			public int Count { get; set; }
+			/// <summary>
+			/// マシン全体のブロック上限
+			/// </summary>
+			[XmlAttribute("machine_block_max")]
+			public int MachineBlockMax { get; set; }
+			/// <summary>
+			/// ロケットの限凸を許容するかどうか
+			/// </summary>
+			[XmlAttribute("allow_rocket_exceed")]
+			public bool AllowRocketExceed { get; set; }
+		}
+
+		[XmlRoot("P1GPRegulations")]
+		public class P1GPRegulations : Element
+		{
+
+			[XmlElement("P1GPRegulation")]
+			public List<P1GPRegulation> P1GPRegulation { get; set; }
+
+			public P1GPRegulation Find(int count) => P1GPRegulation.Find(r => r.Count == count);
+			/// <summary>
+			/// 必須ブロック
+			/// </summary>
+			/// <param name="count"></param>
+			/// <returns></returns>
+			public List<Block> MandatoryBlocks(int count) => Find(count).Block.FindAll(b => 0 < b.Min);
+			/// <summary>
+			/// 制限ブロック
+			/// </summary>
+			/// <param name="count"></param>
+			/// <returns></returns>
+			public List<Block> LimitedBlocks(int count) => Find(count).Block.FindAll(b => b.Min <= 0 && 0 < b.Max);
+			/// <summary>
+			/// 禁止ブロック
+			/// </summary>
+			/// <param name="count"></param>
+			/// <returns></returns>
+			public List<Block> ForbiddenBlocks(int count) => Find(count).Block.FindAll(b => b.Max <= 0);
+		}
+		/// <summary>
+		/// XMLからレギュレーションを読み込む
+		/// </summary>
+		/// <param name="filePath"></param>
+		/// <returns></returns>
+		public static P1GPRegulations Deserialize(string filePath = "Resources/Regulation.xml")
+        {
+			Mod.Log($"Loaded Regulations from modding folder");
+			var result = Modding.ModIO.DeserializeXml<P1GPRegulations>(filePath);
+
+			// 古い順にソート
+			result.P1GPRegulation = result.P1GPRegulation.OrderBy(regulation => regulation.Count).ToList();
+
+			return result;
+        }
+    }
 }
